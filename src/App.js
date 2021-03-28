7514f75a844b0dba35f597236951aabc0d6a8ba0
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-03-28 22:55:51 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
+const testText = "Hello, its a little test text!";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {testText}
     </div>
   );
 }
