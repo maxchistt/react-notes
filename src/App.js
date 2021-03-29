e159922d305e820aaf3e66b97ba5fe2a55e4a5f4
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-03-29 06:41:06 +0300

    add context, delete cards

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import './App.css';
 import React from 'react'
 import CardList from './Cards/CardList'
 import AddCard from './Cards/AddCard'
+import Context from './context'
 
 const testText = "My little cards-app c:";
-const cardsArr = [
+
+const cards_start_arr = [
   { id: 1, completed: false, title: "Card1" },
   { id: 2, completed: true, title: "abcdefg2" },
   { id: 3, completed: false, title: "qwerty3" },
 ];
+
+
 function App() {
+  const [cardsArr, setTodos] = React.useState(cards_start_arr);
+
+  function removeCard(id) {
+    setTodos(cardsArr.filter(todo => todo.id !== id))
+  } 
+
   return (
+    <Context.Provider value={{ removeCard }}>
     <div className="App">
       <header className="p-1 h2 text-center">{testText}</header>
       <main className="p-1">
@@ -21,6 +32,7 @@ function App() {
 
 
     </div>
+    </Context.Provider>
   );
 }
 
--- src/Cards/CardItem.js
+++ src/Cards/CardItem.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
+import Context from '../context'
 
 function CardItem(props) {
+    const { removeCard } = useContext(Context);
     return (
 
         <div className="p-1 col-lg-3 col-md-6" >
@@ -10,7 +12,7 @@ function CardItem(props) {
                 <div className="card-body">
                     <h5 className="card-title">Card Id: {props.card.id}</h5>
                     <p className="card-text">Name: {props.card.title}</p>
-                    <button className="btn btn-light p-0" style={{ width: "1.8em", height: "1.8em", float: "right" }} >&times;</button>
+                    <button className="btn btn-light p-0" style={{ width: "1.8em", height: "1.8em", float: "right" }} onClick={() => removeCard(props.card.id)}>&times;</button>
                 </div>
             </div>
 
