fd41276e0de52152fc0879ad0dbf6ff07d7d5751
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-03-30 06:12:51 +0300

    gridLayoutUpdater

--- src/Cards/CardList.js
+++ src/Cards/CardList.js
@@ -2,31 +2,44 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import CardItem from './CardItem'
 import StackGrid, { transitions } from "react-stack-grid";
+import sizeMe from 'react-sizeme';
+import { createGridUpdater } from '../Content/GridLayoutUpdater'
 
 const { scaleDown } = transitions;
 
+let gridUpdater = createGridUpdater();
+
+function calcWidth() {
+    const smallMonitorWidth = 576
+    const middleMonitorWidth = 768
+    const winWidth = window.innerWidth
+
+    if (winWidth >= middleMonitorWidth) return '33.33%'
+    else if (winWidth >= smallMonitorWidth) return '50%'
+    else return '100%'
+}
+
 function CardList(props) {
     return (
-        <div className="container p-0">
-            <StackGrid
-                appear={scaleDown.appear}
-                appeared={scaleDown.appeared}
-                enter={scaleDown.enter}
-                entered={scaleDown.entered}
-                leaved={scaleDown.leaved}
-            >
-                {props.cards.map((card, index) => {
-                    return (
-                        <CardItem
-                            card={card}
-                            key={card.id}
-                            index={index}
-                        />
-                    )
-                })}
-            </StackGrid>
-        </div>
-
+        <StackGrid className="container p-0"
+            gridRef={gridR => gridUpdater.setGridRef(gridR)}
+            appear={scaleDown.appear}
+            appeared={scaleDown.appeared}
+            enter={scaleDown.enter}
+            entered={scaleDown.entered}
+            leaved={scaleDown.leaved}
+            columnWidth={calcWidth()}
+        >
+            {props.cards.map((card, index) => {
+                return (
+                    <CardItem
+                        card={card}
+                        key={card.id}
+                        index={index}
+                    />
+                )
+            })}
+        </StackGrid>
     )
 }
 
@@ -34,5 +47,4 @@ CardList.propTypes = {
     cards: PropTypes.arrayOf(PropTypes.object).isRequired,
 }
 
-export default CardList
-
+export default sizeMe()(CardList)
