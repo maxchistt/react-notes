fdb77c3038bb7a91489a1905f9fc899d3f4a451c
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-04-21 01:16:55 +0300

    clean fixes

--- src/Cards/CardItem.js
+++ src/Cards/CardItem.js
@@ -4,23 +4,14 @@ import Context from '../context'
 import Card from '../Shared/Card'
 import ReactMarkdown from 'react-markdown'
 import gfm from 'remark-gfm'
-//import Palette from './palette'
-
-function createHTML(text) {
-    let el = document.createElement("p")
-    el.innerText = el.textContent = text
-    return { __html: el.innerHTML }
-}
 
 function CardItem(props) {
-    const { removeCard, /*changeCardColor,*/ setEditCard } = useContext(Context)
+    const { removeCard, setEditCard } = useContext(Context)
     const { card, index } = props
     const cardItem = card && new Card(card)
     const lineClip = 12
     const bgColor = cardItem.color
-    /*function tryChangeColor(color) {
-        changeCardColor(index, color)
-    }*/
+
     return (
 
         <div className="p-1" >
@@ -47,11 +38,6 @@ function CardItem(props) {
                     >
                         &#10007;
                     </button>
-                    {/*<Palette
-                        className={`btn btn-light p-0 mx-2`}
-                        style={{ width: "1.8em", height: "1.8em", float: "right", borderColor: "transparent", backgroundColor: "transparent" }}
-                        setColor={tryChangeColor}
-                    ></Palette>*/}
                 </div>
 
             </div>
