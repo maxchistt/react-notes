563af620023f81e8f4f24da5487ec3fa43b49a62
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-04-20 18:10:24 +0300

    brand text

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Card, { checkCardsArr } from './Shared/Card'
 
 const { loadData, postData, updDataServLogin } = DataService()
 
-const testText = "Notes App"
+const brandText = "Notes"
 
 var cardCount = 0
 
@@ -203,7 +203,7 @@ function App() {
           <nav className="d-flex container px-0 flex-wrap-reverse justify-content-around">
             <div className="text-center d-flex p-1 align-items-center justify-content-center flex-wrap">
               <img src={logo} className="App-logo" alt="logo" />
-              <h1 className="h2 m-0 text-dark pr-3">{testText}</h1>
+              <h1 className="h2 m-0 text-dark pr-3 brand">{brandText}</h1>
             </div>
             <div className="text-center d-flex p-0 align-items-center flex-wrap ml-auto">
               {logged &&
