79e5ad16f9efbdf30c5f05329cb642826e5979e1
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-06-08 22:14:18 +0300

    Models

---
+++ models/Note.js
@@ -0,0 +1,13 @@
+const {Schema, model, Types} = require('mongoose')
+
+const schema = new Schema({
+  id: {type: String, required: true, unique: true},
+  name: {type: String},
+  text: {type: String},
+  color: {type: String},
+  image: {type: String},
+  date: {type: Date, default: Date.now},
+  owner: {type: Types.ObjectId, ref: 'User', required: true}
+})
+
+module.exports = model('Note', schema)
--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const {Schema, model, Types} = require('mongoose')
 
 const schema = new Schema({
   email: {type: String, required: true, unique: true},
-  password: {type: String, required: true}
+  password: {type: String, required: true},
+  notes: [{ type: Types.ObjectId, ref: 'Note' }]
 })
 
 module.exports = model('User', schema)
