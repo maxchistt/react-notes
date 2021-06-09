4f52dfc6540da84d905272d2b5d09a33329a7e12
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-06-09 14:32:45 +0300

    auth style

--- client/src/Pages/AuthPage.js
+++ client/src/Pages/AuthPage.js
@@ -58,84 +58,85 @@ function AuthPage() {
 
   return (
     <div className="AuthPage">
+      <div className="p-1   py-5 container" >
 
+        <div className="form-body  px-5 py-4 bg-light mx-auto">
 
-      <div className="p-1 pb-3 mb-3 container" >
-        <div className=" form-body mx-auto ">
-          <h1 className="text-center">Авторизация</h1>
-          <div className="container p-3 bg-light">
-
-            <div className='bg-light form-group mb-2'>
-              <div className='mb-1'>
-                <span className={auth.isAuthenticated ? `badge badge-success` : `badge badge-secondary`}>
-                  {auth.isAuthenticated ? <span><i className="bi bi-key"> </i>AUTORISED</span> : 'UNAUTORISED'}
-                </span>
-                {auth.email ? <span> {auth.email} </span> : ``}
-              </div>
+          <div className='bg-light form-group mb-2'>
+            <div className='mb-1'>
+              <span className={auth.isAuthenticated ? `badge badge-success` : `badge badge-secondary`}>
+                {auth.isAuthenticated ? <span><i className="bi bi-key"> </i>AUTORISED</span> : 'UNAUTORISED'}
+              </span>
+              {auth.email ? <span> {auth.email} </span> : ``}
             </div>
-
-            <div className='bg-light form-group'>
-              {
-                message &&
-                <div className={`alert alert-${message[1] ? "info" : "danger"} my-1`} role="alert">
-                  <span className={`badge badge-${message[1] ? "info" : "danger"} d-inline mr-2`}>{message[1] ? "Инфо" : "Ошибка"}</span>
-                  <span>{String(message[0])}</span>
-                </div>
-              }
-
-              <label htmlFor="email">Email</label>
-              <input
-                placeholder="Введите email"
-                id="email"
-                type="text"
-                name="email"
-                className="form-control"
-                value={form.email}
-                onChange={changeHandler}
-                readOnly={auth.isAuthenticated}
-                onKeyPress={e => e.key === 'Enter' && loginHandler()}
-              />
-
-              <label htmlFor="email">Пароль</label>
-              <input
-                placeholder="Введите пароль"
-                id="password"
-                type="password"
-                name="password"
-                className="form-control"
-                value={form.password}
-                onChange={changeHandler}
-                readOnly={auth.isAuthenticated}
-                onKeyPress={e => e.key === 'Enter' && loginHandler()}
-              />
-            </div>
-
-            <div className='bg-light form-group form-row'>
-              {!auth.isAuthenticated ? (
-                <React.Fragment>
-
-                  <div className="col-12 col-sm-4 col-md-5 p-1">
-                    <button className="btn btn-primary col" disabled={loading} onClick={loginHandler}><i className="bi bi-person-check"></i> Войти</button>
-                  </div>
-
-                  <div className="col p-1">
-                    <button className="btn btn-outline-primary col" disabled={loading} onClick={registerHandler}><i className="bi bi-person-plus"></i> Регистрация</button>
-                  </div>
-
-                </React.Fragment>
-              ) : (
-                <React.Fragment>
-
-                  <div className="col-12 p-1">
-                    <button className="btn btn-danger col" disabled={loading} onClick={logoutHandler}><i className="bi bi-person-x"></i> Выйти</button>
-                  </div>
-
-                </React.Fragment>
-              )}
-            </div>
-
           </div>
+
+          <div className='bg-light form-group mb-3'>
+            <h1 className="text-center">Авторизация</h1>
+          </div>
+
+          <div className='bg-light form-group'>
+            {
+              message &&
+              <div className={`alert alert-${message[1] ? "info" : "danger"} my-1`} role="alert">
+                <span className={`badge badge-${message[1] ? "info" : "danger"} d-inline mr-2`}>{message[1] ? "Инфо" : "Ошибка"}</span>
+                <span>{String(message[0])}</span>
+              </div>
+            }
+
+            <label htmlFor="email">Email</label>
+            <input
+              placeholder="Введите email"
+              id="email"
+              type="text"
+              name="email"
+              className="form-control mb-1"
+              value={form.email}
+              onChange={changeHandler}
+              readOnly={auth.isAuthenticated}
+              onKeyPress={e => e.key === 'Enter' && loginHandler()}
+            />
+
+            <label htmlFor="email">Пароль</label>
+            <input
+              placeholder="Введите пароль"
+              id="password"
+              type="password"
+              name="password"
+              className="form-control mb-1"
+              value={form.password}
+              onChange={changeHandler}
+              readOnly={auth.isAuthenticated}
+              onKeyPress={e => e.key === 'Enter' && loginHandler()}
+            />
+          </div>
+
+          <div className='bg-light form-group form-row'>
+            {!auth.isAuthenticated ? (
+              <React.Fragment>
+
+                <div className="col-12 col-sm-4 col-md-5 p-1">
+                  <button className="btn btn-primary col" disabled={loading} onClick={loginHandler}><i className="bi bi-person-check"></i> Войти</button>
+                </div>
+
+                <div className="col p-1">
+                  <button className="btn btn-outline-primary col" disabled={loading} onClick={registerHandler}><i className="bi bi-person-plus"></i> Регистрация</button>
+                </div>
+
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+
+                <div className="col-12 p-1">
+                  <button className="btn btn-danger col" disabled={loading} onClick={logoutHandler}><i className="bi bi-person-x"></i> Выйти</button>
+                </div>
+
+              </React.Fragment>
+            )}
+          </div>
+
         </div>
+
       </div>
     </div>
   )
