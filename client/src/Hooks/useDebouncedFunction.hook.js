f3d71ea4563a47a121f175140148a1d0a6f1b5f9
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2021-06-14 04:35:26 +0300

    debounce hook fixup

--- client/src/Hooks/useDebouncedFunction.hook.js
+++ client/src/Hooks/useDebouncedFunction.hook.js
@@ -3,15 +3,15 @@ import { useEffect, useReducer } from 'react';
 
 /**
  * Хук Debounced Function
- * @param {void} callback 
+ * @param {(arg)=>void} callback 
  * @param {number} delayms 
  */
 function useDebouncedFunction(callback, delayms) {
-    const [state, debounced] = useReducer(() => {
-        return { count: state.count++, args: arguments }
-    }, { count: 0, args: undefined })
+    const [state, debounced] = useReducer((state, arg) => {
+        return { count: state.count++, arg: arg }
+    }, { count: 0, arg: undefined })
     useEffect(() => {
-        const handler = setTimeout(() => callback(state.arguments), delayms)
+        const handler = setTimeout(() => callback(state.arg), delayms)
         return () => clearTimeout(handler)
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [state])
